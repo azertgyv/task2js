63299d0cf172c5282956a4564f179f779a7dfe63
azertgyv <atagiyev077@gmail.com>
2023-05-06 20:56:01 +0400

    task2js

---
+++ task2.js
@@ -0,0 +1,46 @@
+
+// Task1
+
+
+// let name1 = prompt('Xos geldiniz. Rutbeinizi daxil edin: ')
+// if (name1 == 'user'){
+//     console.log('Xos geldin isdifadeci')
+// }
+// if (name1 == 'admin'){
+//     console.log('Xos geldin cenab admin')
+// }
+// if (name1=='guest'){
+//     console.log('xos geldin qonaq')
+// }
+
+
+
+// task2
+
+// let ed = prompt('Ededleri daxil edin: ')
+
+// if (ed % 3 == 0 && ed % 5 == 0){
+//     console.log('FizzBuzz')
+// }
+// else if (ed % 3 == 0)
+//    console.log('Fizz')
+// else if (ed % 5 == 0)
+// console.log('Buzz')
+
+//task3 
+
+// let number = prompt('Eded alin: ')
+// if (number >= 10 && number <= 99){
+//     console.log('Eded iki reqemlidir')
+// }
+// else {
+//     console.log('Eded iki reqemli deyildir.')
+// }
+
+//task4
+
+// let num1 = +prompt('Birinci ededi daxil edin: ')
+// let num2 = +prompt('Ikinci ededi daxil edin: ')
+// let num3 = +prompt('Ucuncu ededi daxil edin: ')
+// let sum = num1 + num2 + num3
+// sum > 100 ? console.log('100 u kecdi') : console.log('100 u kecmedi')
